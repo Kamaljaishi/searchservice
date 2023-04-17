35e04da965d337ac35fa0ac5fb3900f7564c0e7e
kamal j <kamaljaishi081@gmail.com>
2023-04-17 21:53:03 +0530

    repository layer added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const {PORT}=require('./config/configenv');
+const  CityRepository =require('./repository/db-repo');
 console.log(PORT);
 const setupServer=async()=>{
     const app=express();
     app.listen(PORT,()=>{
         console.log("Server started at    ",PORT );
+
+        const repo=new CityRepository();
+        repo.createCity({name:"Kathmandu"});
     });
 }
 setupServer();
